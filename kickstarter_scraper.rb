1f6ee285dd99823cdad1d02566d512041888ba64
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-09-23 02:54:42 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,5 +2,8 @@
 require 'nokogiri'
 
 def create_project_hash
-  
+  # This just opens a file and reads it into a variable
+  html = File.read('fixtures/kickstarter.html')
+ 
+  kickstarter = Nokogiri::HTML(html)
 end
