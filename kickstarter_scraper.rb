f3bf704a7054e59e55cfb1339ebff427edbf5c62
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-09-23 03:20:54 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,7 +11,7 @@ require 'pry'
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 
-ddef create_project_hash
+def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
  
